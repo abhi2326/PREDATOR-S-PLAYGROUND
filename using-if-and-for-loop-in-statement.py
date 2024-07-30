f9a07d8031488fdc25dcb67a4fa52ae1a73ca20e
Abhijeet Srivastava <122362018+abhi2326@users.noreply.github.com>
2024-07-30 21:38:09 +0530

    Add files via upload

---
+++ using-if-and-for-loop-in-statement.py
@@ -0,0 +1,15 @@
+#using if loop in the particular variable statement
+n = int(input("Enter the number you want to check: "))
+number = "pass" if n >= 10 else "fail"
+print(number)
+
+#using for loop for printing the names accordingly
+a='vishant'
+for i in a:
+    print(a)
+
+#or
+a='vishant'
+for i in a:
+    print(i)
+
